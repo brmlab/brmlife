2603aacf1f9ca2b081b598ce46810abd9bbc28b5
Petr Baudis <pasky@ucw.cz>
2011-11-26 23:37:25 +0100

    Don't die on SIGPIPE

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include <cstring>
 
 #include <netinet/in.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -22,6 +23,7 @@ main(int argc, char *argv[])
 
 	srandom(time(NULL));
 
+	signal(SIGPIPE, SIG_IGN);
 	int lfd = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in sin;
 	memset(&sin, 0, sizeof(sin));
