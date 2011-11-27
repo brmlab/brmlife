9bf8281b913953681dc13da4207c7d28b6037b64
Petr Baudis <pasky@ucw.cz>
2011-11-27 04:16:32 +0100

    dead_body_energy: Scale down from 500 to 200

--- world.h
+++ world.h
@@ -8,7 +8,7 @@ struct world {
 	const static int defense_cost = -20;
 	const static int sun_energy = 1;
 
-	const static int dead_body_energy = 500;
+	const static int dead_body_energy = 200;
 	const static double dead_body_energy_carryover = 0.5;
 	const static int dead_decay = -1;
 };
