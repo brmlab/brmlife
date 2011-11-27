a2a52f5884c2d7a9fdd09bac64458969e1bf76f0
Petr Baudis <pasky@ucw.cz>
2011-11-27 04:41:41 +0100

    World: Multiply all constants by ten for higher granularity

--- world.h
+++ world.h
@@ -2,15 +2,15 @@
 #define BRMLIFE__WORLD_H
 
 struct world {
-	const static int newborn_energy = 500;
-	const static int move_cost = -5;
-	const static int attack_cost = -40;
-	const static int defense_cost = -20;
-	const static int sun_energy = 1;
+	const static int newborn_energy = 5000;
+	const static int move_cost = -50;
+	const static int attack_cost = -400;
+	const static int defense_cost = -200;
+	const static int sun_energy = 10;
 
-	const static int dead_body_energy = 200;
+	const static int dead_body_energy = 2000;
 	const static double dead_body_energy_carryover = 0.5;
-	const static int dead_decay = -1;
+	const static int dead_decay = -10;
 };
 
 #endif
